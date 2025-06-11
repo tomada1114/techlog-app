bbd22ab54519295c678fbee9282e4fb1be4f6d10
tomada <68495563+tomada1114@users.noreply.github.com>
2025-06-11 09:33:42 -0600

    コメント機能のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
 
-  resources :posts, only: [:index, :new, :create, :show, :destroy]
+  resources :posts, only: [:index, :new, :create, :show, :destroy] do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+
   resources :users, only: [:show]
 end
